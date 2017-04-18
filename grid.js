4b4a46a1a36e09db878ca5b3db652183ada5864f
David Bass <doctordake@gmail.com>
2017-04-17 22:09:14 -0500

    Added random color toggle

--- grid.js
+++ grid.js
@@ -1,17 +1,63 @@
 jQuery(document).ready(function(){
+  sketchPad();
+
+});
+
+function sketchPad(){
+  createGrid();
+  random = false;
+  draw(random);
+  setRandom();
+  erase();
+}
+
+function draw(rand){
+    $(".grid").mouseenter(function(){
+      if(rand){
+        rgb = randomColor();
+      } else {
+        rgb = [0,0,0];
+      }
+      $(this).css("background-color", "rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")");
+  });
+}
+
+function setRandom(){
+    $(".random").click(function(){
+      random = !random;
+      if(random){
+          $(this).html("Random colors are ON");
+          draw(true);
+      } else {
+        $(this).html("Random colors are OFF");
+        draw(false);
+      }
+    });
+}
+
+function randomColor(){
+  r = Math.floor(Math.random()*255);
+  g = Math.floor(Math.random()*255);
+  b = Math.floor(Math.random()*255);
+  return [r,g,b];
+}
+
+function erase(){
+    $(".erase").click(function(){
+      size = prompt("How many pixels tall/wide should the grid be?");
+      createGrid(size);
+    });
+}
+
+function createGrid(size = 16){
+  $(".container").remove();
   $("body").append("<div></div>");
   $("div").addClass("container");
-  for(i=0;i<16;i++){
-    for(j=0;j<16;j++){
-        $(".container").append("<div class='grid'></div>");
+  container = $(".container");
+  for(i=0;i<size;i++){
+    for(j=0;j<size; j++){
+        container.append("<div class='grid'></div>");
     }
-    $(".container").append("<br/>");
+    container.append("<br/>");
   }
-  $(".grid").mouseenter(function(){
-    $(this).css("background-color", "black");
-  });
-
-  $(".erase").click(function(){
-    $("div").css("background-color", "white");
-  });
-});
+}
